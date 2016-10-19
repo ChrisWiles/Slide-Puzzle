83f5a095da82780eb6a53256a26990c8f59309f8
Christopher Wiles <chriswiles87@gmail.com>
2016-10-19 17:37:07 -0500

    refactor

--- src/Components/BoardDisplay.jsx
+++ src/Components/BoardDisplay.jsx
@@ -4,38 +4,39 @@ import Paper from 'material-ui/Paper'
 // This class handles the button that the app displays
 class BoardDisplay extends Component {
 
-  render() {
-
-    // let makeMove = this.props.makeMove
-    // conversion of the 1-d array to 2-d
-    let oldBoard = this.props.board.slice(0)
-    let newBoard = []
-    while(oldBoard.length) {
-      newBoard.push(oldBoard.splice(0, this.props.N))
-    }
-
-    // generating the markup for the 2-d array
-    let board = newBoard.map(function boardMarkup(row) {
-      let rowMarkup = row.map(function (element) {
-        if (element !== 0) {
-          return (
-            <Paper zDepth={1} className='board-cell' key={element}>
-              <p>{element}</p>
-            </Paper>
-          )
-        }
-        else {
-          return <div className='board-cell' key={element}></div>
-        }
-      })
-      return <row className='board-row' key={row}>{rowMarkup}</row>
+  mapBoard(board) {
+    return board.map(row => {
+       let rowMarkup = row.map(element => {
+          if (element !== 0) {
+             return (
+                <Paper zDepth={1} className='board-cell' key={element}>
+                   <p>{element}</p>
+                </Paper>
+             )
+          } else {
+             return <div className='board-cell' key={element}></div>
+          }
+       })
+       return <row className='board-row' key={row}>{rowMarkup}</row>
     })
-
-    // returning the markup
-    return (
-      <div className='board'>{board}</div>
-    )
   }
+
+   render() {
+      const {N, board} = this.props
+
+      // conversion of the 1-d array to 2-d
+      let oldBoard = board.slice(0)
+      const newBoard = []
+
+      while (oldBoard.length) {
+         newBoard.push(oldBoard.splice(0, N))
+      }
+
+      const mappedBoard = this.mapBoard(newBoard)
+      // generating the markup for the 2-d array
+
+      return <div className='board'>{mappedBoard}</div>
+   }
 }
 
 export default BoardDisplay
