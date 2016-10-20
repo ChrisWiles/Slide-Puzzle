1a72e19bdd52384cc38c3a02c3d1efa9faef3800
Christopher Wiles <chriswiles87@gmail.com>
2016-10-20 08:00:47 -0500

    intergrate counter into TopBar

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -1,13 +1,65 @@
-import React, {Component} from 'react'
-import AppBar from 'material-ui/AppBar'
+import React, {Component, PropTypes} from 'react'
+
+import IconMenu from 'material-ui/IconMenu'
+import IconButton from 'material-ui/IconButton'
+import FontIcon from 'material-ui/FontIcon'
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more'
+import MenuItem from 'material-ui/MenuItem'
+import DropDownMenu from 'material-ui/DropDownMenu'
+import RaisedButton from 'material-ui/RaisedButton'
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar'
+
 
 class TopBar extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 3,
+    }
+  }
+
+  handleChange = (event, index, value) => this.setState({value})
+
   render() {
+    const {reset, count} = this.props
     return (
-      <AppBar title='N-Puzzle' />
+      <Toolbar>
+        <ToolbarGroup firstChild={true}>
+          <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+            <MenuItem value={1} primaryText="All Broadcasts" />
+            <MenuItem value={2} primaryText="All Voice" />
+            <MenuItem value={3} primaryText="8-Puzzle" />
+            <MenuItem value={4} primaryText="Complete Voice" />
+            <MenuItem value={5} primaryText="Complete Text" />
+            <MenuItem value={6} primaryText="Active Voice" />
+            <MenuItem value={7} primaryText="Active Text" />
+          </DropDownMenu>
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <ToolbarTitle text={`Moves: ${count}`} />
+          <FontIcon className="muidocs-icon-custom-sort" />
+          <ToolbarSeparator />
+          <RaisedButton label="Reset Game" secondary={true} onClick={reset}/>
+          <IconMenu
+            iconButtonElement={
+              <IconButton touch={true}>
+                <NavigationExpandMoreIcon />
+              </IconButton>
+            }
+          >
+            <MenuItem primaryText="Sign In" />
+            <MenuItem primaryText="Top Scores" />
+          </IconMenu>
+        </ToolbarGroup>
+      </Toolbar>
     )
   }
+}
+
+TopBar.propTypes = {
+  reset: PropTypes.func.isRequired,
+  count: PropTypes.number.isRequired
 }
 
 export default TopBar
