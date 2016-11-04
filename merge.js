7d96ce72d2e5827a1a4fb6dde3681fa3fcf929ae
Christopher Wiles <chriswiles87@gmail.com>
2016-11-04 11:36:29 -0500

    test merge function

---
+++ merge.js
@@ -0,0 +1,156 @@
+let Mongo = { N3:
+   [ { name: 'Chris', time: '00:00:01:000' },
+     { name: 'Nathan', time: '20:00:00:000' },
+     { name: 'Jen', time: '00:00:03:000' },
+     { name: 'Kai', time: '00:00:04:000' },
+     { name: 'A5', time: '00:00:05:000' },
+     { name: 'Bob', time: '00:00:06:000' },
+     { name: 'Luke', time: '00:00:07:000' },
+     { name: 'Sam', time: '00:81:00:000' },
+     { name: 'Matt', time: '00:90:00:000' },
+     { name: 'Mario', time: '10:00:00:000' } ],
+  N8:
+   [ { name: 'Chris', time: '00:00:01:000' },
+     { name: 'Nathan', time: '20:00:00:000' },
+     { name: 'Jen', time: '00:00:03:000' },
+     { name: 'Kai', time: '00:00:04:000' },
+     { name: 'A5', time: '00:00:05:000' },
+     { name: 'Bob', time: '00:00:06:000' },
+     { name: 'Luke', time: '00:00:07:000' },
+     { name: 'Sam', time: '00:81:00:000' },
+     { name: 'Matt', time: '00:90:00:000' },
+     { name: 'Mario', time: '10:00:00:000' } ],
+  N15:
+   [ { name: 'Chris', time: '00:00:01:000' },
+     { name: 'Nathan', time: '20:00:00:000' },
+     { name: 'Jen', time: '00:00:03:000' },
+     { name: 'Kai', time: '00:00:04:000' },
+     { name: 'A5', time: '00:00:05:000' },
+     { name: 'Bob', time: '00:00:06:000' },
+     { name: 'Luke', time: '00:00:07:000' },
+     { name: 'Sam', time: '00:81:00:000' },
+     { name: 'Matt', time: '00:90:00:000' },
+     { name: 'Mario', time: '10:00:00:000' } ],
+  N24:
+   [ { name: 'Chris', time: '00:00:01:000' },
+     { name: 'Nathan', time: '20:00:00:000' },
+     { name: 'Jen', time: '00:00:03:000' },
+     { name: 'Kai', time: '00:00:04:000' },
+     { name: 'A5', time: '00:00:05:000' },
+     { name: 'Bob', time: '00:00:06:000' },
+     { name: 'Luke', time: '00:00:07:000' },
+     { name: 'Sam', time: '00:81:00:000' },
+     { name: 'Matt', time: '00:90:00:000' },
+     { name: 'Mario', time: '10:00:00:000' } ],
+  N35:
+   [ { name: 'Chris', time: '00:00:01:000' },
+     { name: 'Nathan', time: '20:00:00:000' },
+     { name: 'Jen', time: '00:00:03:000' },
+     { name: 'Kai', time: '00:00:04:000' },
+     { name: 'A5', time: '00:00:05:000' },
+     { name: 'Bob', time: '00:00:06:000' },
+     { name: 'Luke', time: '00:00:07:000' },
+     { name: 'Sam', time: '00:81:00:000' },
+     { name: 'Matt', time: '00:90:00:000' },
+     { name: 'Mario', time: '10:00:00:000' } ],
+  N48:
+   [ { name: 'Chris', time: '00:00:01:000' },
+     { name: 'Nathan', time: '20:00:00:000' },
+     { name: 'Jen', time: '00:00:03:000' },
+     { name: 'Kai', time: '00:00:04:000' },
+     { name: 'A5', time: '00:00:05:000' },
+     { name: 'Bob', time: '00:00:06:000' },
+     { name: 'Luke', time: '00:00:07:000' },
+     { name: 'Sam', time: '00:81:00:000' },
+     { name: 'Matt', time: '00:90:00:000' },
+     { name: 'Mario', time: '10:00:00:000' } ] }
+
+let Client = { N3:
+   [ { name: 'Chris', time: '00:00:01:000' },
+     { name: 'Fastest', time: '00:00:00:500' },
+     { name: 'Jen', time: '00:00:03:000' },
+     { name: 'Kai', time: '00:00:04:000' },
+     { name: 'A5', time: '00:00:05:000' },
+     { name: 'Bob', time: '00:00:06:000' },
+     { name: 'Luke', time: '00:00:07:000' },
+     { name: 'Sam', time: '00:81:00:000' },
+     { name: 'Matt', time: '00:90:00:000' },
+     { name: 'Mario', time: '10:00:00:000' } ],
+  N8:
+   [ { name: 'Chris', time: '00:00:01:000' },
+     { name: 'Fastest', time: '00:00:00:500' },
+     { name: 'Jen', time: '00:00:03:000' },
+     { name: 'Kai', time: '00:00:04:000' },
+     { name: 'A5', time: '00:00:05:000' },
+     { name: 'Bob', time: '00:00:06:000' },
+     { name: 'Luke', time: '00:00:07:000' },
+     { name: 'Sam', time: '00:81:00:000' },
+     { name: 'Matt', time: '00:90:00:000' },
+     { name: 'Mario', time: '10:00:00:000' } ],
+  N15:
+   [ { name: 'Chris', time: '00:00:01:000' },
+     { name: 'Fastest', time: '00:00:00:500' },
+     { name: 'Jen', time: '00:00:03:000' },
+     { name: 'Kai', time: '00:00:04:000' },
+     { name: 'A5', time: '00:00:05:000' },
+     { name: 'Bob', time: '00:00:06:000' },
+     { name: 'Luke', time: '00:00:07:000' },
+     { name: 'Sam', time: '00:81:00:000' },
+     { name: 'Matt', time: '00:90:00:000' },
+     { name: 'Mario', time: '10:00:00:000' } ],
+  N24:
+   [ { name: 'Chris', time: '00:00:01:000' },
+     { name: 'Fastest', time: '00:00:00:500' },
+     { name: 'Jen', time: '00:00:03:000' },
+     { name: 'Kai', time: '00:00:04:000' },
+     { name: 'A5', time: '00:00:05:000' },
+     { name: 'Bob', time: '00:00:06:000' },
+     { name: 'Luke', time: '00:00:07:000' },
+     { name: 'Sam', time: '00:81:00:000' },
+     { name: 'Matt', time: '00:90:00:000' },
+     { name: 'Mario', time: '10:00:00:000' } ],
+  N35:
+   [ { name: 'Chris', time: '00:00:01:000' },
+     { name: 'Fastest', time: '00:00:00:500' },
+     { name: 'Jen', time: '00:00:03:000' },
+     { name: 'Kai', time: '00:00:04:000' },
+     { name: 'A5', time: '00:00:05:000' },
+     { name: 'Bob', time: '00:00:06:000' },
+     { name: 'Luke', time: '00:00:07:000' },
+     { name: 'Sam', time: '00:81:00:000' },
+     { name: 'Matt', time: '00:90:00:000' },
+     { name: 'Mario', time: '10:00:00:000' } ],
+  N48:
+   [ { name: 'Chris', time: '00:00:01:000' },
+     { name: 'Fastest', time: '00:00:00:500' },
+     { name: 'Jen', time: '00:00:03:000' },
+     { name: 'Kai', time: '00:00:04:000' },
+     { name: 'A5', time: '00:00:05:000' },
+     { name: 'Bob', time: '00:00:06:000' },
+     { name: 'Luke', time: '00:00:07:000' },
+     { name: 'Sam', time: '00:81:00:000' },
+     { name: 'Matt', time: '00:90:00:000' },
+     { name: 'Mario', time: '10:00:00:000' } ] }
+
+// const names = 'Chris Fastest Jen Kai A5 Bob Luke Sam Matt Mario'.split(' ')
+// const times = '00:00:01:000 00:00:00:500 00:00:03:000 00:00:04:000 00:00:05:000 00:00:06:000 00:00:07:000 00:81:00:000 00:90:00:000 10:00:00:000'.split(' ')
+
+const merge = (Mongo, Client) => {
+  const cleanTime = (time) => time.time.split(':').join('')
+  const leaderBoard = {}
+  for (let key in Mongo) {
+    let scores = leaderBoard[key] = []
+
+    Client[key].forEach((score, i) => {
+      scores.push(score)
+      if (Mongo[key][i].time !== score.time) {
+        scores.push(Mongo[key][i])
+      }
+    })
+    scores.sort((a, b) => cleanTime(a) - cleanTime(b)).splice(10)
+  }
+  return leaderBoard
+}
+
+
+console.log(merge(Mongo, Client))
